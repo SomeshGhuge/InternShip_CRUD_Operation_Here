8c188daa51471ae75ec7f1898de45817ecf82535
HP <someshghuge85@gmail.com>
2022-10-12 11:56:21 +0530

    Test

---
+++ test/src/main/java/com/test/Array.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Array {
+	public static void main(String[]args) {
+		
+	
+		try {
+			int a[]= {0,1,2,3};
+			System.out.println(a[3]);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
