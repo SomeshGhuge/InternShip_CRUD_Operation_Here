e5ae4869923ee781b503420e57146e3f08f8fbc1
HP <someshghuge85@gmail.com>
2022-10-12 12:36:21 +0530

    Setter Based Injection

---
+++ Setter_Base_Injection/src/main/java/com/test/Student.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Student {
+
+	private String city;
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void getMessage(String name) {
+		System.out.println("Hello,"+name+","+city);
+}}
